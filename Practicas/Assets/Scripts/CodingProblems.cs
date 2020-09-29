d6f8c3cf438bb45a369113f454cc854955f563a0
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-09-29 12:45:48 -0700

    Coding problem

---
+++ Practicas/Assets/Scripts/CodingProblems.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingProblems : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] nums = {8,1,2,2,3};
+        SmallerNumbers(nums);
+    }
+
+    public int[] SmallerNumbers(int[] nums){
+        Debug.Log("Solving problems");
+        return nums;
+    }
+
+
+}
