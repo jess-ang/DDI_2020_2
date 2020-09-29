327114a74508c5dacef016562825fb812e4d12d3
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-09-29 16:03:32 -0700

    Practica 1 'CodingProblems' agregada

--- Practicas/Assets/Scripts/CodingProblems.cs
+++ Practicas/Assets/Scripts/CodingProblems.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class CodingProblems : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         int[] nums = {8,1,2,2,3};
-        SmallerNumbers(nums);
+        printArray(SmallerNumbers(nums));
     }
 
     public int[] SmallerNumbers(int[] nums){
-        Debug.Log("Solving problems");
-        return nums;
+        int[] numsCnt = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if(nums[j]<nums[i])
+                    numsCnt[i]++;
+            }
+        }
+        return numsCnt;
     }
 
-
+    public void printArray(int[] nums){
+        foreach (int i in nums) 
+        {
+            Debug.Log(i);
+        }
+    }
 }
