0e298256a24c4f67f2be79e429ce2e40c400a91e
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-10-16 22:17:31 -0700

    Moneda agregada

---
+++ Practicas/Assets/Scripts/Prac3/Coin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : Interactable
+{
+    Rigidbody rb;
+    public float torque;
+    bool interactionSet = false;
+    
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if(interactionSet)
+        {
+            rb.AddTorque(transform.up * torque * -1f);
+        }
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        interactionSet = true;
+    }
+}
