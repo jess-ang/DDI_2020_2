d5436b66926a90e78839f9dd17215083e439e6d1
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-10-16 20:12:49 -0700

    Codigo problema P3

---
+++ Practicas/Assets/Scripts/Prac3/Gallina.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gallina : Interactable
+{
+    Rigidbody rb;
+    public Vector3 jumpDirection;
+    public float jumpForce = 30f;
+    private AudioSource source;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        if (rb != null)
+        {
+            source.Play();
+            Destroy(gameObject,1);
+            rb.AddForce(jumpDirection * jumpForce, ForceMode.Force);
+        }
+    }
+}
---
+++ Practicas/Assets/Scripts/Prac3/Interactable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    bool isInsideZone = false;
+
+    public virtual void Interact()
+    {
+        Debug.Log("Ejecutando interaccion...");
+    }
+
+    void Update()
+    {
+        if(isInsideZone && Input.GetKeyDown(KeyCode.I))
+        {
+            Interact();
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+        isInsideZone = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+        isInsideZone = false;
+    }
+}
