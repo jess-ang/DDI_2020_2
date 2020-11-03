6960ded010140b8588f8d34e952d4987bad1f409
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-11-02 17:44:08 -0700

    Inventory added

---
+++ Practicas/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject inventoryUIPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventoryUIPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            inventoryUIPanel.SetActive(!inventoryUIPanel.activeSelf);
+        }
+    }
+}
