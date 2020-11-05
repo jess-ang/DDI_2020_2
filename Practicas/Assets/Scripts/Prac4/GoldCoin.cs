43d2bd579bedd516d6ec50757a5952ef7cf09aa8
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-11-04 16:47:54 -0800

    Barra de vida disminuyendo

---
+++ Practicas/Assets/Scripts/Prac4/GoldCoin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldCoin : Interactable
+{
+    Rigidbody rb;
+    private AudioSource source;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        Debug.Log("Recogiendo moneda...");
+        if (!source.isPlaying)
+        {
+            source.Play();
+        }
+        Destroy(gameObject,1f);
+    }
+}
