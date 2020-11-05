efaa25cd389eea391a2f3061328a14a631061f3e
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-11-05 14:43:47 -0800

    Armas agregadas

--- Practicas/Assets/Scripts/Pistol.cs
+++ Practicas/Assets/Scripts/Pistol.cs
@@ -9,21 +9,9 @@ public class Pistol : Item
     public float reloadTime = 0.5f;
     public int maxCapacity = 7;
     public float caliber = 10f;
-        public int energyPoints = 10;
-
-        private GameObject player;
 
     public override void Use()
     {
-
         Debug.Log("Shooting pistol "+name+". Reloading in "+reloadTime);
-
-                player = GameObject.Find("MainPlayer");
-        Health health = player.GetComponent<Health>();
-        if (health != null)
-        {
-        Debug.Log("Eating " +name+ ". Increasing energy: +" +energyPoints);
-            health.ModifyHealth(-energyPoints);
-        }
     } 
 }
