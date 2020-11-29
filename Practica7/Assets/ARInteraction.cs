5dd55d4dbfd55851ebf894d27fb8b81cea5e7ba3
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-11-29 15:59:18 -0800

    Practica 7 AR terminada

---
+++ Practica7/Assets/ARInteraction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARInteraction : MonoBehaviour
+{
+    public Answer targetAnswer;
+    public bool answerType;
+    private AudioSource source;
+
+    void OnMouseDown()
+    {
+        source = GetComponent<AudioSource>();
+        source.Play();
+        if(targetAnswer != null)
+        {
+            targetAnswer.ChangeMaterial(answerType);
+        }
+        // Debug.Log("Capturando");
+        // Destroy(this.gameObject);
+    }
+}
---
+++ Practica7/Assets/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Answer : MonoBehaviour
+{
+    public Material rightAnswerMaterial;
+    public Material wrongAnswerMaterial;
+    private Renderer myRenderer;
+
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+    }
+
+    public void ChangeMaterial(bool answer)
+    {
+        myRenderer.material = answer ? rightAnswerMaterial : wrongAnswerMaterial;
+    }
+    
+}
